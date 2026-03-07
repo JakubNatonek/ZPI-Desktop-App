528730ebc9dbd26987b0342200999e4373478710
Jakub Natonek <natonek32@gmail.com>
2026-03-07 20:47:49 +0100

    First Commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'zpi.desktop.app',
+  appName: 'zpi-desktop-app',
+  webDir: 'www'
+};
+
+export default config;
