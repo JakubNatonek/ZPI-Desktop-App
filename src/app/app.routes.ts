3bb4908860a7ac30f76b3a9045022d6b9e5c2539
Michaltbk <100926282+Michaltbk@users.noreply.github.com>
2026-03-16 19:54:39 +0100

    Dodano możliwość dodawania sal dla administratora

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -22,6 +22,10 @@ export const routes: Routes = [
     loadComponent: () => import('./profile/profile.page').then((m) => m.ProfilePage),
   },
   {
+    path: 'sale',
+    loadComponent: () => import('./sale/sale.page').then((m) => m.SalePage),
+  },
+  {
     path: '',
     redirectTo: 'login',
     pathMatch: 'full',
